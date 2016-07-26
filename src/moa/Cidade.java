e2bc43cf200cb1ce96b4d768773c0ad914e2d315
guest-NyVQjg <guest-NyVQjg@DINSL010-109168>
2016-07-25 22:33:03 -0300

    Implementado permutação do conj inicial

---
+++ src/moa/Cidade.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moa;
+
+/**
+ *
+ * @author guest-NyVQjg
+ */
+class Cidade {
+
+    protected String nome;
+    protected int x = 0;
+    protected int y = 0;
+}
---
+++ src/moa/Moa.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moa;
+
+/**
+ *
+ * @author guest-NyVQjg
+ */
+public class Moa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Conjunto entrada 
+        Conjunto conjunto = new Conjunto();
+
+        // Conjunto Incial Permutado 
+        conjunto.cnjPermutado = conjunto.getPermutacaoCnjCidade(conjunto.getConjuntoCidade());
+
+
+        
+    }
+
+}
